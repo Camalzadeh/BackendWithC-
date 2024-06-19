caf9e03a14c428c1cac78e12e2c7b8277abd1b08
Hümbət Camalov <143912059+Camalzadeh@users.noreply.github.com>
2024-06-19 15:26:36 +0400

    I add 2nd Tasks folder and write solutions of tasks here

---
+++ 2nd Tasks/Task2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿List<String> AIleBaslayanlar(List<String> adlar)
+{
+    List<String> secilmisAdlar = new List<String>();
+    foreach (String ad in adlar)
+    {
+        if (ad.StartsWith('a') || ad.StartsWith('A'))
+        {
+            secilmisAdlar.Add(ad);
+        }
+    }
+    return secilmisAdlar;
+}
---
+++ 2nd Tasks/Task2.3/Program.cs
@@ -0,0 +1,23 @@
+﻿bool Polindrom(int num)
+{
+    return num.ToString().Reverse().ToArray().SequenceEqual(num.ToString().ToArray());
+}
+
+int num;
+Console.WriteLine("Please enter a 4 digit number");
+String input = Console.ReadLine();
+if (int.TryParse(input, out num))
+{
+    if (num > 999 && num < 10000)
+    {
+        Console.WriteLine(Polindrom(num));
+    }
+    else
+    {
+        Console.WriteLine("False");
+    }
+}
+else
+{
+    Console.WriteLine("False");
+}
